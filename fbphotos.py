9931e112134ff41c2a4ca3d92daa73227b9dfc49
Matt DeBoard <matt.deboard@gmail.com>
2011-07-01 15:15:46 -0900

    Added subdirectory creation to map to facebook album name

--- fbphotos.py
+++ fbphotos.py
@@ -1,21 +1,30 @@
+import optparse
 import os
+import re
+import subprocess
+import sys
 import urllib2
 
 import facepy
 
 from mytoken import token, username
 
-def get_photos(dl_dir=os.getcwd()):
+def get_photos(dl_dir):
+    dest = os.path.abspath(dl_dir)
+    p = re.compile(r"[,!'\ /]")
     fb_photos = find_photos()
-    for album_id in fb_photos:
-        for img_url in fb_photos[album_id]:
+    for album in fb_photos:
+        albname = p.sub("_", album).lower()
+        mk_album_dirs(dest, albname)
+        folder = albname
+        for img_url in fb_photos[album]['images']:
             img_name = img_url.split('/')[-1]
             url = urllib2.urlopen(img_url)
             
-            with open("%s/%s" % (dl_dir, img_name), 'w') as f:
+            with open("%s/%s/%s" % (dest, folder, img_name), 'w') as f:
                 meta = url.info()
                 filesize = int(meta.getheaders("Content-Length")[0])
-                print "Downloading: %s Bytes: %s" % (img_name, filesize)
+                #print "Downloading: %s Bytes: %s" % (img_name, filesize)
                 filesize_dl = 0
                 blocksize = 8192
                 while True:
@@ -28,24 +37,40 @@ def get_photos(dl_dir=os.getcwd()):
                     status = r"%10d [%3.2f%%]" % (filesize_dl,
                                                   filesize_dl * 100. / filesize)
                     status = status + chr(8)*(len(status)+1)
-                    print status,
+                    #print status,
     
 def find_photos():
     '''
     Creates a dictionary, with album id as key and a list of images
     in the album as the value.
     '''
-    photos = {}
+    albums = {}
     graph = facepy.GraphAPI(token)
     my_albums = graph.get("%s/albums" % username)
-    album_ids = [a['id'] for a in my_albums]
-    for aid in album_ids:
-        my_pics = graph.get("%s/photos?limit=100" % aid)
-        pic_urls = [pic['source'] for pic in my_pics]
-        photos[aid] = pic_urls
+    for album in my_albums:
+        albums[album['name']] = {}
+        albums[album['name']]['id'] = album['id']
+        my_pics = graph.get("%s/photos?limit=100" % album['id'])
+        albums[album['name']]['images'] = [pic['source'] for pic in my_pics]
+    return albums
 
-    return photos
-
+def mk_album_dirs(dest, album):
+    '''
+    Create a subfolder for each facebook album.
+    '''
+    if not os.path.exists("%s/%s" % (dest, album)):
+        os.mkdir("%s/%s" % (dest, album))
+    return
 
 if __name__ == "__main__":
-    get_photos()
+    d = os.getcwd()
+    parser = optparse.OptionParser()
+    parser.add_option("-d", "--dest", action="store", type="string",
+                      dest="dest_dir", default=os.getcwd(),
+                      help=("Specify the directory where you want your photos t"
+                            "o be downloaded. This directory must already exist"
+                            ". Photos will be downloaded to current working dir"
+                            " by default."))
+    args = sys.argv[1:]
+    (options, args) = parser.parse_args(args)
+    get_photos(options.dest_dir)
--- mytoken.py
+++ mytoken.py
@@ -1,2 +1,3 @@
-token = ''
-username = ''
+token = ("2227470867|2.AQCtegMLNgCc1vMl.3600.1309572000.0-1255534407|DbOvqeKmma"
+         "BR3t6781EatiUXilc")
+username = 'matt.deboard'
